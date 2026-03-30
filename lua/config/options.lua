8a60869314ebb5bb2a9dd64e731fa8cd2317d3b2
Bekzat Ajan <bekzat@synthefy.com>
2026-03-30 13:47:42 +0500

    copying

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,21 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Use OSC 52 for clipboard over SSH (copies yanked text to local machine clipboard)
+if os.getenv("SSH_TTY") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
+
 -- Detect macOS system appearance and set background accordingly
 local function get_system_appearance()
   local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
