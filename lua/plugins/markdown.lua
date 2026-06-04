98dbe8ded08419b5862762360c6fe54851f3559f
Bekzat Ajan <bekzat@synthefy.com>
2026-06-04 22:22:34 +0500

    markdown prev

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -6,6 +6,10 @@ return {
     },
     opts = function()
       vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_port = "8765" -- fixed port so SSH forwarding is predictable
+      vim.g.mkdp_echo_preview_url = 1 -- print the URL instead of auto-opening a browser
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_browser = "" -- don't try to launch a browser on the remote
     end,
   },
 }
